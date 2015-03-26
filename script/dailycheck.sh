3acc4ae07d1b6ae7eb1aca508d27054a229b740a
Stephen Schrauger <stephen@schrauger.com>
2015-03-26 10:08:56 -0400

    fixes crontab relative path bug

--- script/dailycheck.sh
+++ script/dailycheck.sh
@@ -1,12 +1,14 @@
 # 1. Download daily manual, and save filename with date.
-current_date=`date +%y-%m-%d`
-elite_full_current="ELITE-DANGEROUS-GAME-MANUAL current.pdf"
-elite_quick_current="ELITE-DANGEROUS-MANUAL current.pdf"
-elite_full_daily="ELITE-DANGEROUS-GAME-MANUAL $current_date.pdf" 
-elite_quick_daily="ELITE-DANGEROUS-MANUAL $current_date.pdf" 
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+DIR="$DIR/"
+log_dir=$DIR"log.txt"
+current_date=`date +%Y-%m-%d`
+elite_full_current="$DIR""ELITE-DANGEROUS-GAME-MANUAL current.pdf"
+elite_quick_current="$DIR""ELITE-DANGEROUS-MANUAL current.pdf"
+elite_full_daily="$DIR""ELITE-DANGEROUS-GAME-MANUAL $current_date.pdf" 
+elite_quick_daily="$DIR""ELITE-DANGEROUS-MANUAL $current_date.pdf" 
 elite_full_url="http://hosting.zaonce.net/elite/website/assets/ELITE-DANGEROUS-GAME-MANUAL.pdf"
 elite_quick_url="http://hosting.zaonce.net/elite/website/assets/ELITE-DANGEROUS-MANUAL.pdf"
-
 wget --quiet -O "$elite_full_daily" "$elite_full_url"
 wget --quiet -O "$elite_quick_daily" "$elite_quick_url"
 
@@ -16,37 +18,52 @@ wget --quiet -O "$elite_quick_daily" "$elite_quick_url"
  # 3b. Create simlink to daily manual.
 
 # full manual
-if [ ! -f "$elite_full_current" ]; then
+if [ ! -L "$elite_full_current" ]; then
   # no 'current' exists. link to daily and continue.
-  ln -s "$elite_full_daily" "$elite_full_current";
-  echo "$current_date Missing Symlink - Full" >> log.txt
+  ln -sf "$elite_full_daily" "$elite_full_current";
+  echo "$current_date Missing Symlink - Full" >> $log_dir
 else
   # current exists. compare with daily.
-  diff "$elite_full_current" "$elite_full_daily"  >> log.txt
-  if [ $? -ne 0 ]; then
-    # newest version differs. save and update
-    rm "$elite_full_current"; #only removes symlink, not actual file.
-    ln -s "$elite_full_daily" "$elite_full_current";
-    echo "$current_date New Version - Full" >> log.txt
+  if [ -f "$elite_full_daily" ]; then
+    diff "$elite_full_current" "$elite_full_daily"  >> $log_dir
+    if [ $? -ne 0 ]; then
+      # newest version differs. save and update
+      rm "$elite_full_current"; #only removes symlink, not actual file.
+      ln -sf "$elite_full_daily" "$elite_full_current";
+      echo "$current_date New Version - Full" >> $log_dir
+    else
+      # newest version is the same. delete daily download.
+      rm "$elite_full_daily";
+      echo "$current_date No Change - Full" >> $log_dir
+    fi;
   else
-    # newest version is the same. delete daily download.
-    rm "$elite_full_daily";
-    echo "$current_date No Change - Full" >> log.txt
+    echo 'file not exists'
+    # had a malfunction. either wget failed to get the file,
+    # or some other error (permissions, hd space, etc) caused
+    # the new daily to not download properly
+    echo "$elite_full_daily not found. Filesystem error. Check permissions." >> $log_dir
   fi;
 fi;
 
 # quick start manual
-if [ ! -f "$elite_quick_current" ]; then
-  ln -s "$elite_quick_daily" "$elite_quick_current";
-  echo "$current_date Missing Symlink - Quick" >> log.txt
+if [ ! -L "$elite_quick_current" ]; then
+  ln -sf "$elite_quick_daily" "$elite_quick_current";
+  echo "$current_date Missing Symlink - Quick" >> $log_dir
 else
-  diff "$elite_quick_current" "$elite_quick_daily";
-  if [ $? -ne 0 ]; then
-    rm "$elite_quick_current";
-    ln -s "$elite_quick_daily" "$elite_quick_current";
-    echo "$current_date New Version - Quick" >> log.txt
+  if [ -f "$elite_quick_daily" ]; then
+    diff "$elite_quick_current" "$elite_quick_daily";
+    if [ $? -ne 0 ]; then
+      rm "$elite_quick_current";
+      ln -sf "$elite_quick_daily" "$elite_quick_current";
+      echo "$current_date New Version - Quick" >> $log_dir
+    else
+      rm "$elite_quick_daily";
+      echo "$current_date No Change - Quick" >> $log_dir
+    fi;
   else
-    rm "$elite_quick_daily";
-    echo "$current_date No Change - Quick" >> log.txt
+    # had a malfunction. either wget failed to get the file,
+    # or some other error (permissions, hd space, etc) caused
+    # the new daily to not download properly
+    echo "$elite_quick_daily not found. Filesystem error. Check permissions." >> $log_dir
   fi;
 fi;
