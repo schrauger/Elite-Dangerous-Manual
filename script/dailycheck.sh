2a23390d02659e8a59000db3750d2e3c74e88515
Stephen Schrauger <stephen@schrauger.com>
2015-04-10 10:35:49 -0400

    splits log file to two separate logs

--- script/dailycheck.sh
+++ script/dailycheck.sh
@@ -8,7 +8,8 @@
 # 1. Download daily manual, and save filename with date.
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 DIR="$DIR/"
-log_dir=$DIR"log.txt"
+log_dir_full=$DIR"log_full.txt"
+log_dir_quick=$DIR"log_quick.txt"
 current_date=`date +%Y-%m-%d`
 elite_full_current="$DIR""ELITE-DANGEROUS-GAME-MANUAL current.pdf"
 elite_quick_current="$DIR""ELITE-DANGEROUS-MANUAL current.pdf"
@@ -28,45 +29,45 @@ wget --quiet -O "$elite_quick_daily" "$elite_quick_url"
 if [ ! -L "$elite_full_current" ]; then
   # no 'current' exists. link to daily and continue.
   ln -sf "$elite_full_daily" "$elite_full_current";
-  echo "$current_date Missing Symlink - Full" >> $log_dir
+  echo "$current_date Missing Symlink - Full" >> $log_dir_full
 else
   # current exists. compare with daily.
   if [ -f "$elite_full_daily" ]; then
-    diff "$elite_full_current" "$elite_full_daily"  >> $log_dir
+    diff "$elite_full_current" "$elite_full_daily" >> $log_dir_full
     if [ $? -ne 0 ]; then
       # newest version differs. save and update
       rm "$elite_full_current"; #only removes symlink, not actual file.
       ln -sf "$elite_full_daily" "$elite_full_current";
-      echo "$current_date New Version - Full" >> $log_dir
+      echo "$current_date New Version - Full" >> $log_dir_full
     else
       # newest version is the same. delete daily download.
       rm "$elite_full_daily";
-      echo "$current_date No Change - Full" >> $log_dir
+      echo "$current_date No Change - Full" >> $log_dir_full
     fi;
   else
     # had a malfunction. either wget failed to get the file,
     # or some other error (permissions, hd space, etc) caused
     # the new daily to not download properly
-    echo "$elite_full_daily not found. Filesystem error. Check permissions." >> $log_dir
+    echo "$elite_full_daily not found. Filesystem error. Check permissions." >> $log_dir_full
   fi;
 fi;
 
 # quick start manual
 if [ ! -L "$elite_quick_current" ]; then
   ln -sf "$elite_quick_daily" "$elite_quick_current";
-  echo "$current_date Missing Symlink - Quick" >> $log_dir
+  echo "$current_date Missing Symlink - Quick" >> $log_dir_quick
 else
   if [ -f "$elite_quick_daily" ]; then
-    diff "$elite_quick_current" "$elite_quick_daily";
+    diff "$elite_quick_current" "$elite_quick_daily" >> $log_dir_quick
     if [ $? -ne 0 ]; then
       rm "$elite_quick_current";
       ln -sf "$elite_quick_daily" "$elite_quick_current";
-      echo "$current_date New Version - Quick" >> $log_dir
+      echo "$current_date New Version - Quick" >> $log_dir_quick
     else
       rm "$elite_quick_daily";
-      echo "$current_date No Change - Quick" >> $log_dir
+      echo "$current_date No Change - Quick" >> $log_dir_quick
     fi;
   else
-    echo "$elite_quick_daily not found. Filesystem error. Check permissions." >> $log_dir
+    echo "$elite_quick_daily not found. Filesystem error. Check permissions." >> $log_dir_quick
   fi;
 fi;
